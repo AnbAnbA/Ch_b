b4dadd726cc712f9d50d211750654e89a6749ab4
БыковаАА <protocolapogay@gmail.com>
2023-03-10 15:51:29 +0300

    commit1

--- app/src/main/java/com/example/ch_b/SplashScreen.java
+++ app/src/main/java/com/example/ch_b/SplashScreen.java
@@ -4,13 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
-
     private final int SPLASH_DISPLAY_LENGTH = 2000;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +29,4 @@ public class SplashScreen extends AppCompatActivity {
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
-
-
 }
